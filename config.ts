0059d830e0a5a7ade85776ce63a25665e72ea289
ammo <solidifyarmor@gmail.com>
2024-02-25 02:39:54 +0630

    fix: load .env and Deno.env.get

--- config.ts
+++ config.ts
@@ -1,15 +1,16 @@
-import { loadSync } from "https://deno.land/std@0.217.0/dotenv/mod.ts";
+import { load } from "https://deno.land/std@0.217.0/dotenv/mod.ts";
 
-const env = loadSync();
+await load();
 
-export const PORT = Number(env.PORT) || 4000;
+export const PORT = Number(Deno.env.get("PORT")) ?? 3000;
 
-export const APP_URL = env.APP_URL;
+export const APP_URL = Deno.env.get("APP_URL") ?? `http://localhost:${PORT}`;
 
-export const AWS_ACCESS_KEY_ID = env.AWS_ACCESS_KEY_ID;
-export const AWS_SECRET_ACCESS_KEY = env.AWS_SECRET_ACCESS_KEY;
-export const AWS_REGION = env.AWS_REGION;
-export const AWS_S3_BUCKET_NAME = env.AWS_S3_BUCKET_NAME;
+export const AWS_ACCESS_KEY_ID = Deno.env.get("AWS_ACCESS_KEY_ID") ?? "";
+export const AWS_SECRET_ACCESS_KEY =
+  Deno.env.get("AWS_SECRET_ACCESS_KEY") ?? "";
+export const AWS_REGION = Deno.env.get("AWS_REGION") ?? "";
+export const AWS_S3_BUCKET_NAME = Deno.env.get("AWS_S3_BUCKET_NAME") ?? "";
 
 console.log({
   PORT,
