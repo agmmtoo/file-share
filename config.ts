acbb519a877d4f7053f44e0a9e6c171002118b1c
ammo <solidifyarmor@gmail.com>
2024-02-25 02:20:02 +0630

    debug: log env vars

--- config.ts
+++ config.ts
@@ -11,4 +11,15 @@ export const APP_URL = env.APP_URL;
 export const AWS_ACCESS_KEY_ID = env.AWS_ACCESS_KEY_ID;
 export const AWS_SECRET_ACCESS_KEY = env.AWS_SECRET_ACCESS_KEY;
 export const AWS_REGION = env.AWS_REGION;
-export const AWS_S3_BUCKET_NAME = env.AWS_S3_BUCKET_NAME;
+export const AWS_S3_BUCKET_NAME = env.AWS_S3_BUCKET_NAME || "ammo-file-share";
+
+console.log({
+    PORT,
+    HTML_INDEX,
+    HTML_DOWNLOAD,
+    APP_URL,
+    AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY,
+    AWS_REGION,
+    AWS_S3_BUCKET_NAME,
+})
